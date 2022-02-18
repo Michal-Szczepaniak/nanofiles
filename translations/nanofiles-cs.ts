a626189c4e5a0e8779de930f0de75beea2703094
Jozef Mlich <jozef.mlich@greycortex.com>
2022-02-18 16:50:59 +0100

    add czech translation

---
+++ translations/nanofiles-cs.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>O aplikaci</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak. Based on fileman.</source>
+        <translation>Vytvořil Michał Szczepaniak. Na základě aplikace fileman.</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>Licencováno pod GPLv3</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Odkazy</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Poděkování</translation>
+    </message>
+    <message>
+        <source>Rudi Timmermans for fileman.</source>
+        <translation>Rudimu Timmermansovi za fileman.</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Přispět</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Nanofiles</source>
+        <translation>Nanofiles</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <source>Okay</source>
+        <translation>Okej</translation>
+    </message>
+</context>
+<context>
+    <name>FileInfo</name>
+    <message>
+        <source>Share</source>
+        <translation>Sdílet</translation>
+    </message>
+    <message>
+        <source>Size:</source>
+        <translation>Velikost:</translation>
+    </message>
+    <message>
+        <source>Modified:</source>
+        <translation>Upraveno:</translation>
+    </message>
+    <message>
+        <source>Accessed:</source>
+        <translation>Přístup:</translation>
+    </message>
+</context>
+<context>
+    <name>FileProcess</name>
+    <message>
+        <source>Installing...</source>
+        <translation>Instaluje se...</translation>
+    </message>
+    <message>
+        <source>Opening file...</source>
+        <translation>Otevírá soubor...</translation>
+    </message>
+    <message>
+        <source>Installing packages</source>
+        <translation>Instaluje se balíček</translation>
+    </message>
+    <message>
+        <source>Downloading packages</source>
+        <translation>Stahuje se balíček</translation>
+    </message>
+    <message>
+        <source>Installed</source>
+        <translation>Nainstalováno</translation>
+    </message>
+    <message>
+        <source>Nothing was done</source>
+        <translation>Nic se neprovedlo</translation>
+    </message>
+    <message>
+        <source>Installation failed</source>
+        <translation>Instalace selhala</translation>
+    </message>
+    <message>
+        <source>Installation was started</source>
+        <translation>Instalace začala</translation>
+    </message>
+    <message>
+        <source>Installation couldn&apos;t be started</source>
+        <translation>Nelze spustit instalaci</translation>
+    </message>
+    <message>
+        <source>File opened</source>
+        <translation>Soubor byl otevřen</translation>
+    </message>
+    <message>
+        <source>Suitable application was not found</source>
+        <translation>Nebyla nalezena vhodná aplikace</translation>
+    </message>
+    <message>
+        <source>File couldn&apos;t be opened</source>
+        <translation>Soubor se nepodařilo otevřít</translation>
+    </message>
+</context>
+<context>
+    <name>FileRenameDialog</name>
+    <message>
+        <source>Rename</source>
+        <translation>Přejmenovat</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>About</source>
+        <translation>O aplikaci</translation>
+    </message>
+    <message>
+        <source>New…</source>
+        <translation>Nový...</translation>
+    </message>
+    <message>
+        <source>Add to places</source>
+        <translation>Přidat do míst</translation>
+    </message>
+    <message>
+        <source>Hide hidden files</source>
+        <translation>Skrýt skryté soubory</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>Zobrazit skryté soubory</translation>
+    </message>
+    <message>
+        <source>Switch to grid view</source>
+        <translation>Přepnout na mřížku</translation>
+    </message>
+    <message>
+        <source>Switch to list view</source>
+        <translation>Přepnout na seznam</translation>
+    </message>
+    <message>
+        <source>Restart in user mode</source>
+        <translation>Restartovat v uživatelském režimu</translation>
+    </message>
+    <message>
+        <source>Restart in root mode</source>
+        <translation>Restartovat v režimu roota</translation>
+    </message>
+    <message>
+        <source>Size: %1 Modified: %2</source>
+        <translation>Velikost: %1 Změněno: %2</translation>
+    </message>
+</context>
+<context>
+    <name>NewFilesDialog</name>
+    <message>
+        <source>Directory</source>
+        <translation>Složka</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Soubor</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <source>Name…</source>
+        <translation>Jméno...</translation>
+    </message>
+</context>
+<context>
+    <name>Places</name>
+    <message>
+        <source>Delete</source>
+        <translation>Smazat</translation>
+    </message>
+    <message>
+        <source>Move Up</source>
+        <translation>Posunout Nahoru</translation>
+    </message>
+    <message>
+        <source>Move Down</source>
+        <translation>Posunout dolů</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Přejmenovat</translation>
+    </message>
+    <message>
+        <source>Change name</source>
+        <translation>Změnit jméno</translation>
+    </message>
+    <message>
+        <source>Reset entries</source>
+        <translation>Vynulovat hodnoty</translation>
+    </message>
+</context>
+<context>
+    <name>ShareFilesPage</name>
+    <message>
+        <source>No sharing accounts available!</source>
+        <translation>Nejsou k dispozici účty sdílení!</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Sdílet</translation>
+    </message>
+</context>
+<context>
+    <name>Worker</name>
+    <message>
+        <source>Copying files...</source>
+        <translation>Kopírují se soubory...</translation>
+    </message>
+    <message>
+        <source>Creating directories...</source>
+        <translation>Vytváří se složka...</translation>
+    </message>
+    <message>
+        <source>Directories couldn&apos;t be created.</source>
+        <translation>Složky nelze vytvořit.</translation>
+    </message>
+    <message>
+        <source>Copying files (%1 of %2)...</source>
+        <translation>Kopírují se soubory (%1 z %2)...</translation>
+    </message>
+    <message>
+        <source>All files were copied successfully.</source>
+        <translation>Všechny soubory byly zkopírování.</translation>
+    </message>
+    <message>
+        <source>All files couldn&apos;t be copied successfully.</source>
+        <translation>Nelze zkopirovat všechny soubory.</translation>
+    </message>
+    <message>
+        <source>Deleting old files (%1 of %2)...</source>
+        <translation>Mažou se staré soubory (%1 z %2)...</translation>
+    </message>
+    <message>
+        <source>Deleting old directories...</source>
+        <translation>Mažou se staré složky...</translation>
+    </message>
+    <message>
+        <source>All of the files were cut and pasted successfully.</source>
+        <translation>Všechny soubory byli vyjmuty a vloženy.</translation>
+    </message>
+    <message>
+        <source>All of the files couldn&apos;t be cut and pasted successfully.</source>
+        <translation>Všechny soubory nejde vyjmout a vložit.</translation>
+    </message>
+    <message>
+        <source>Failed to copy all files, deleting copied files (%1 of %2)...</source>
+        <translation>Selhalo kopírování souborů, mažou se nakopírované soubory (%1 z %2)...</translation>
+    </message>
+    <message>
+        <source>Failed to copy all files, deleting created directories...</source>
+        <translation>Selhalo kopírování souborů, mažou se vytvořené složky...</translation>
+    </message>
+    <message>
+        <source>Deleting files...</source>
+        <translation>Mažou se soubory...</translation>
+    </message>
+    <message>
+        <source>Deleting files (%1 of %2)...</source>
+        <translation>Mažou se soubory (%1 z %2)...</translation>
+    </message>
+    <message>
+        <source>Deleting directories...</source>
+        <translation>Maže se složka...</translation>
+    </message>
+    <message>
+        <source>All files were deleted successfully.</source>
+        <translation>Všechny soubory byly smazány.</translation>
+    </message>
+    <message>
+        <source>All files couldn&apos;t be deleted successfully.</source>
+        <translation>Nelze smazat všechny soubory.</translation>
+    </message>
+</context>
+</TS>
